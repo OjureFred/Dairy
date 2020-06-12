17e5e8e359929d377c3b76a4a3194a51e0003c6f
Fred Ojure <fredojure@hotmail.com>
2020-06-12 11:40:38 +0300

    Added DiaryItem class

---
+++ src/app/diary-item.spec.ts
@@ -0,0 +1,7 @@
+import { DiaryItem } from './diary-item';
+
+describe('DiaryItem', () => {
+  it('should create an instance', () => {
+    expect(new DiaryItem()).toBeTruthy();
+  });
+});
---
+++ src/app/diary-item.ts
@@ -0,0 +1,7 @@
+export class DiaryItem {
+    id: number;
+    title: string;
+    date: Date;
+    startTime: string;
+    duration: number;
+}
