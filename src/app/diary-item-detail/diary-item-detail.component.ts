6d2b5749046b6ec4addc004a12329b6830d206ee
Fred Ojure <fredojure@hotmail.com>
2020-06-12 15:24:14 +0300

    updated code for diary detail

--- src/app/diary-item-detail/diary-item-detail.component.ts
+++ src/app/diary-item-detail/diary-item-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { DiaryItem } from './../diary-item';
 
 @Component({
   selector: 'app-diary-item-detail',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./diary-item-detail.component.css']
 })
 export class DiaryItemDetailComponent implements OnInit {
+
+  @Input() diaryItem: DiaryItem;
 
   constructor() { }
 
